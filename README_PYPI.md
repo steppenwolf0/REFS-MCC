75f90400e4f14156ec71808986620b71ff269fa1
Brigitta <vrg.brigitta@gmail.com>
2026-07-18 23:01:10 +0200

    Easier import

---
+++ README_PYPI.md
@@ -0,0 +1,40 @@
+# REFS-MCC
+Recursive Ensemble Feauture Selection using Matthews Correlation Coefficient
+
+--------------------------------------------------------------------
+Installation
+
+```bash
+   pip install refs-mcc
+```
+
+After installation it can be used from CLI, for more information, run:
+```bash
+   refs-mcc --help
+```
+
+Or from python code:
+```python
+    from refs_mcc import REFC_MCC
+
+    if __name__ == '__main__': 
+        REFC_MCC().run()
+```
+
+--------------------------------------------------------------------
+Input
+
+`data` folder with 
+- `data_0.csv`
+- `features_0.csv`
+- `ids.csv`
+- `labels.csv`
+
+--------------------------------------------------------------------
+Output
+
+- run folders (`run0`, `run1`, ..., `run{n-1}`, where n is the selected number of total runs, 10 by default)
+- `best` folder 
+- `sumFig.pdf` & `sumFig.png`
+
+--------------------------------------------------------------------
--- src/refs_mcc.py
+++ src/refs_mcc.py
@@ -5,21 +5,26 @@ from aBioInf100 import main as part1
 from summaryMulti import fakeBootStrapper as part2
 from sumFig import create_summary_figure as part3
 
-def main(args):
-    threads = args.threads
-    totalRuns = args.totalRuns
-    numberOfFolds = args.folds
+class REFC_MCC:
+    def __init__(self, threads: int = 10, totalRuns: int = 10, numberOfFolds: int = 10):
+        self.threads = threads
+        self.totalRuns = totalRuns
+        self.numberOfFolds = numberOfFolds
 
-    part1(threads, totalRuns, numberOfFolds)
-    part2(totalRuns, numberOfFolds)
-    part3(totalRuns)
-    return
+    def run(self):
+        part1(self.threads, self.totalRuns, self.numberOfFolds)
+        part2(self.totalRuns, self.numberOfFolds)
+        part3(self.totalRuns)
 
-if __name__ == "__main__" :
-	parser = argparse.ArgumentParser(description="REFS-MCC")
-	parser.add_argument('--threads', type=int, default=10, help='Number of threads (default: 10)')
-	parser.add_argument('--totalRuns', type=int, default=10, help='Total number of runs (default: 10)')
-	parser.add_argument('--folds', type=int, default=10, help='Number of folds (default: 10)')
-	args = parser.parse_args()
+def main():
+    parser = argparse.ArgumentParser(description="REFS-MCC")
+    parser.add_argument('--threads', type=int, default=10, help='Number of threads (default: 10)')
+    parser.add_argument('--totalRuns', type=int, default=10, help='Total number of runs (default: 10)')
+    parser.add_argument('--folds', type=int, default=10, help='Number of folds (default: 10)')
+    args = parser.parse_args()
 
-	sys.exit( main(args) )
+    refc_mcc = REFC_MCC(args.threads, args.totalRuns, args.folds)
+    return refc_mcc.run()
+
+if __name__ == "__main__":
+    sys.exit(main())
